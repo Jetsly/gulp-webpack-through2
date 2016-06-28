f70a2b44ab1cb15343c01fa1fa66ae3f5ee915ce
diandi <cnljli@live.com>
2016-06-28 23:00:20 +0800

    fix

--- index.js
+++ index.js
@@ -6,19 +6,36 @@ var statsOptions = {
 
 }
 
-module.exports = function(options, done) {
+module.exports = function (options, done) {
 
     var entry = [];
 
     function transformFunction(file, encoding, callback) {
-        console.log(file.history);
-        console.log(JSON.stringify(file));
-        this.push(file);
+        entry.push(file.history[0])
         callback();
     }
 
     function flushFunction(cb) {
-        console.log(1);
+        var self=this;
+        console.log(entry);
+        var wp = webpack(Object.assign(options, {
+            entry: entry
+        }), function (err, stat) {
+            if (err) {
+                console.log(err);
+            }
+            // console.log(stat);
+        });
+        wp.compiler.plugin('after-emit', function (compilation, callback) {
+            Object.keys(compilation.assets).forEach(function (outname) {
+                if (compilation.assets[outname].emitted) {
+
+                    // var file = prepareFile(fs, compiler, outname);
+                    // self.push(file);
+                }
+            });
+            // callback();
+        });
         // cb();
     }
 
